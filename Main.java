84828e92bac234e78818249dd763b6099765f65f
Budad9807 <Budad9807@wrdsb.ca>
2020-10-07 13:48:59 +0000

    Find out the balance over 5 years

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *A program that will ask the user how much they want to invest with a interest rate
+ * @author David
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a variable that means there balance
+    double bal = 900.50;
+    double rate = 0.10;
+    int years = 5;
+    // Now from there balance,rate and how many years they want to invest
+    for(int i = 1;i <= 5; i= i+1 ){
+     // you have now find the interest
+     double interest = bal * rate;
+     // from here you have to find the final balance
+     bal = bal + interest;
+    }
+    // now you have add the final balance + bal
+    System.out.println("Your final balance" + bal);
+  }
+
     
   }
-}
+
